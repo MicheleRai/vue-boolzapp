f3c0513c7219bb20a08011973f433f36df00145a
MicheleRai <rainonemichele1999@gmail.com>
2022-11-17 18:50:30 +0100

    messages improve

--- js/script.js
+++ js/script.js
@@ -189,7 +189,28 @@ var app = new Vue({
         ],
         },
     ],
-    selectedUser: {},
+    selectedUser: {
+      name: 'Edoardo',
+      avatar: '_1',
+      visible: true,
+      messages: [
+      {
+      date: '10/01/2020 15:30:55',
+      message: 'Hai portato a spasso il cane?',
+      status: 'sent'
+      },
+      {
+      date: '10/01/2020 15:50:00',
+      message: 'Ricordati di stendere i panni',
+      status: 'sent'
+      },
+      {
+      date: '10/01/2020 16:15:22',
+      message: 'Tutto fatto!',
+      status: 'received'
+      }
+      ],
+      },
     index:0,
     input:0,
     searchInput:"",
@@ -197,6 +218,7 @@ var app = new Vue({
     methods:{
       profileActive(contact){
         this.selectedUser = contact;
+        console.table(selectedUser  );
       },
       searchContact(){
         this.contacts.forEach((contact) => {
